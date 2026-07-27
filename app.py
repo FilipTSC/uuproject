1c696c2e3063d88fbc474596bab31fd6b7ccbfe1
FilipTSC <filip.szwerluga@gmail.com>
2026-07-27 11:58:59 +0100

    feature/UU-3 File structure changes and FAQ into About

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/membership')
@@ -13,9 +13,9 @@ def membership():
     return render_template('membership.html')
 
 
-@app.route('/faqs')
-def faqs():
-    return render_template('faqs.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 @app.route('/contact')
